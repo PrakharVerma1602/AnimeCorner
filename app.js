e8c9679fa3fadfbb29dbd2bc40a1e75654b6135e
Prakhar Verma <81768332+PrakharVerma1602@users.noreply.github.com>
2023-10-04 20:14:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const itemNumber = movieLists[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    const ratio = Math.floor(window.innerWidth / 270);
+    clickCounter++;
+    if (itemNumber - (4 + clickCounter) + (4 - ratio) >= 0) {
+      movieLists[i].style.transform = `translateX(${
+        movieLists[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieLists[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+
+  console.log(Math.floor(window.innerWidth / 270));
+});
+
+var movieModal;
+
+document.addEventListener("DOMContentLoaded", function() {
+  movieModal = new bootstrap.Modal(document.getElementById('movieModal'), {
+    keyboard: false
+  })
+});  
+
+function openModalPopup(data) {
+  const videoURL = data.previousElementSibling.dataset.video;
+  const description = data.previousElementSibling.innerText;
+  const title = data.previousElementSibling.previousElementSibling.innerText;
+  movieModal.toggle();
+  document.querySelector(".movie-metadata h4").innerText = title;
+  document.querySelector(".movie-metadata p").innerText = description;
+  document.querySelector(".video-container video").src = videoURL;
+}
+
+
+
+// var myModal = document.getElementById('myModal')
+// var myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', function () {
+//   myInput.focus()
+// })
